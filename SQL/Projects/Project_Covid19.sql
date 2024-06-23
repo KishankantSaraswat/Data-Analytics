16616c310dca280cd3229dd0573bfcf376119112
Krishankant Saraswat <122577473+KishankantSaraswat@users.noreply.github.com>
2024-06-24 00:46:13 +0530

    Update Project_Covid19.sql

--- SQL/Projects/Project_Covid19.sql
+++ SQL/Projects/Project_Covid19.sql
@@ -85,3 +85,132 @@ GROUP BY
 ORDER BY 
     TotalDeathCount DESC;
 
+---Global Number
+
+select sum(new_cases) as totalCases,sum(cast(new_deaths as int)) as totalDeaths,  sum(cast(new_deaths as int))/sum(New_cases)*100 as DeathPercentage--total_cases,total_deaths,(total_deaths/total_cases) as DeathPercentage
+from CovidDeaths$
+--where location like '%India%'
+where continent is not null
+--group by date
+order by 1,2
+
+
+----------------------------------------------------------------------------------------------------------------------------------------
+
+select * from CovidVaccinations$
+
+select * from CovidDeaths$
+Join CovidVaccinations$
+On CovidDeaths$.location=CovidVaccinations$.location
+and CovidDeaths$.date=CovidVaccinations$.date
+
+
+--Looking at Total Population vs Vaccinations
+
+SELECT 
+    cd.continent,
+    cd.location,
+    cd.date,
+    cd.population,
+    cv.new_vaccinations,
+    SUM(CAST(cv.new_vaccinations AS INT)) OVER (PARTITION BY cd.location ORDER BY cd.location, cd.date) AS RollingPeopleVaccinated,
+    (SUM(CAST(cv.new_vaccinations AS INT)) OVER (PARTITION BY cd.location ORDER BY cd.location, cd.date) / cd.population) * 100 AS VaccinationPercentage
+FROM 
+    CovidDeaths$ AS cd
+JOIN 
+    CovidVaccinations$ AS cv
+ON 
+    cd.location = cv.location
+AND 
+    cd.date = cv.date
+WHERE 
+    cd.continent IS NOT NULL
+ORDER BY 
+    cd.location, cd.date;
+
+
+
+---Using CTE
+
+WITH PopvsVac (Continent, Location, Date, Population, New_Vaccinations, RollingPeopleVaccinated) AS
+(
+    SELECT 
+        cd.continent,
+        cd.location,
+        cd.date,
+        cd.population,
+        cv.new_vaccinations,
+        SUM(CAST(cv.new_vaccinations AS INT)) OVER (PARTITION BY cd.location ORDER BY cd.date) AS RollingPeopleVaccinated
+    FROM
+        CovidDeaths$ AS cd
+    JOIN 
+        CovidVaccinations$ AS cv
+    ON 
+        cd.location = cv.location
+        AND cd.date = cv.date
+    WHERE 
+        cd.continent IS NOT NULL
+)
+SELECT *,(RollingPeopleVaccinated/Population)*100
+FROM PopvsVac;
+
+
+
+---Temp Table
+drop table if exists #PercentPopulationVaccinated
+create Table #PercentPopulationVaccinated
+(
+Continent nvarchar(255),
+Location nvarchar(255),
+Date datetime,
+Population numeric,
+New_vaccinations numeric,
+RollingPeopleVaccinated numeric
+)
+
+Insert into #PercentPopulationVaccinated
+SELECT 
+        cd.continent,
+        cd.location,
+        cd.date,
+        cd.population,
+        cv.new_vaccinations,
+        SUM(CAST(cv.new_vaccinations AS INT)) OVER (PARTITION BY cd.location ORDER BY cd.date) AS RollingPeopleVaccinated
+    FROM
+        CovidDeaths$ AS cd
+    JOIN 
+        CovidVaccinations$ AS cv
+    ON 
+        cd.location = cv.location
+        AND cd.date = cv.date
+    WHERE 
+        cd.continent IS NOT NULL
+
+
+SELECT *,(RollingPeopleVaccinated/Population)*100
+FROM #PercentPopulationVaccinated;
+
+
+--creating view to store data for later visualizations
+
+create view PercentPopulationVaccinated as
+SELECT 
+        cd.continent,
+        cd.location,
+        cd.date,
+        cd.population,
+        cv.new_vaccinations,
+        SUM(CAST(cv.new_vaccinations AS INT)) OVER (PARTITION BY cd.location ORDER BY cd.date) AS RollingPeopleVaccinated
+    FROM
+        CovidDeaths$ AS cd
+    JOIN 
+        CovidVaccinations$ AS cv
+    ON 
+        cd.location = cv.location
+        AND cd.date = cv.date
+    WHERE 
+        cd.continent IS NOT NULL
+
+select * from PercentPopulationVaccinated
+
+
